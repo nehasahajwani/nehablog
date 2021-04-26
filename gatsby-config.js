40ade3b8ed122b353f0db815bc308ecd05a6f35d
nehasahajwani <nehasahajwani1@gmail.com>
2021-04-26 10:26:16 +0530

    change author name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Neha Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Neha Sahajwani`,
+      summary: `who lives and works in Bangalore building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
